be7fdfb0725351657f254f0af544a063fa1f691a
Amanj1 <amanj5@msn.com>
2018-11-18 18:29:35 +0100

    generating two files for windowsize positions

---
+++ Python_script/scriptWindow.py
@@ -0,0 +1,27 @@
+import sys
+
+# python scriptWindow chr1:20 5 1 ########example####
+
+def WriteToFile(fileName, strData, s):
+	fileName = fileName +"_" + s +".bed"
+	f=open(fileName, "a+")
+	f.write(strData)
+	f.close()
+	return None
+
+
+def main():
+	Pos = sys.argv[1]
+	window = sys.argv[2]
+	suffix = sys.argv[3]
+	arrPos = Pos.split(':')
+	Up = int(arrPos[1]) + int(window)
+	Down = int(arrPos[1]) - int(window)
+	Down1 = Down +1
+	Up1 = Up+1
+	outputUP = str(arrPos[0]) + '\t' + str(Up)  +'\t' + str(Up1)
+	outputDown = str(arrPos[0]) + '\t' + str(Down)  +'\t' + str(Down1)
+	
+	WriteToFile("tmpUP", outputUP, suffix)
+	WriteToFile("tmpDown", outputDown, suffix)
+main()
