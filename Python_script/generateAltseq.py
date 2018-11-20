334557e281f700499542c63f44892273eef4536d
Amanj1 <amanj5@msn.com>
2018-11-20 14:12:42 +0100

    Finished
    generate altSeq from refSeq.
    This should be used in a bash script
    appending it to a fasta file containing the header.

--- Python_script/generateAltseq.py
+++ Python_script/generateAltseq.py
@@ -1,20 +1,107 @@
 import sys
+# COMMAND: Python generateAltseq.py 'nucleotide' 'ReferenceSequence'
+# OUTPUT: String with new necleotide inserted in posisiton 'window+position+window' 
 
-# Not finshed Yet"
+def StringToArray(x):
+	seq = [y for y in x]
+	return seq
 
+def ArrayToString(x):
+	str1 = ''.join(str(e) for e in x)
+	return str1
 
+def FindPosMid(x):
+	#Sequences are always odd numbers (windowSize*2+1)
+	#Assumtion sequences will never start from position in the begining of chr or the end of it. 
+	pos = (len(x)/2)
+	return pos
 
-def ReadFromFile(FileName, s):
-	FileName = FileName+"_"+s+".text"
-	file = open(FileName, “r”): 
-	print(file.read())
-	f.close()
-	return None
+def InsertInSeq(seq,pos,n):
+	pos = int(pos)
+	seq[pos] = n
+	return seq
 
 def main():
-	suffix = sys.argv[1]
-	query = sys.argv[2]
-	
-	ReadFromFile("tmpUP", suffix)
-	
+	nucleotide = sys.argv[1]
+	sequence = sys.argv[2]
+	seqArr = StringToArray(sequence)
+	p = FindPosMid(seqArr)
+	newSeqArr = InsertInSeq(seqArr,p,nucleotide)
+	altSeq = ArrayToString(newSeqArr)
+	print(altSeq)
+	return
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
