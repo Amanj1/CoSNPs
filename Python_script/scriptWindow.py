94c18411cf1b6a4fb3f68852db1714f0f960ca3b
Amanj1 <amanj5@msn.com>
2018-11-20 20:27:14 +0100

    Inclduing files for refSeq positions
    Read comments in file

--- Python_script/scriptWindow.py
+++ Python_script/scriptWindow.py
@@ -2,6 +2,12 @@ import sys
 
 # python scriptWindow chr1:20 5 1 ########example####
 # Python scriptWindow 'position in chr' 'window size' 'index when generating tmp files'
+# WriteToFile("tmpUP", outputUP, suffix) - Generates positions for PacBio reads UpStream
+# WriteToFile("tmpDown", outputDown, suffix)  - Generates positions for PacBio reads DownStream
+# WriteToFile("tmpPosRef", CoverRegion, suffix) - Generates positions for reference sequence
+# When using COMMAND after generating files
+# bedtools getfasta -fi sequenceHG19.fasta -bed tmpPosRef_'suffix'.bed -fo 'nameForRefSeq'.fa
+
 
 def WriteToFile(fileName, strData, s):
 	fileName = fileName +"_" + s +".bed"
@@ -17,12 +23,16 @@ def main():
 	suffix = sys.argv[3]
 	arrPos = Pos.split(':')
 	Up = int(arrPos[1]) + int(window)
-	Down = int(arrPos[1]) - int(window)
+	Down = int(arrPos[1]) - int(window) -1
 	Down1 = Down +1
 	Up1 = Up+1
 	outputUP = str(arrPos[0]) + '\t' + str(Up)  +'\t' + str(Up1)
 	outputDown = str(arrPos[0]) + '\t' + str(Down)  +'\t' + str(Down1)
+	CoverRegion = str(arrPos[0]) + '\t' + str(Down)  +'\t' + str(Up)
 	
 	WriteToFile("tmpUP", outputUP, suffix)
 	WriteToFile("tmpDown", outputDown, suffix)
+	WriteToFile("tmpPosRef", CoverRegion, suffix)
+	return
+	
 main()
