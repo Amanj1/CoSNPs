19e56823cb0ff9c0f8808d2fa472b7f5c26bf4e5
zhiwei <li120415323@gmail.com>
2019-03-07 22:27:04 +0100

    hide debug line

--- reAli/SelectPacBio.sh
+++ reAli/SelectPacBio.sh
@@ -5,5 +5,6 @@ scriptWindowPacBio.py $1 $2 $3
 bedtools intersect -abam Longread_Selected.bam -b tmpUP_$3.bed  -wa \
 | bedtools intersect -abam stdin -b tmpDown_$3.bed  -wa \
 > Longread_Selected_tmp.bam
+#samtools view Longread_Selected_tmp.bam | wc -l
 mv -f Longread_Selected_tmp.bam Longread_Selected.bam
 rm tmpDown* tmpUP*
